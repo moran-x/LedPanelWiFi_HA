2491b4a0e4c009b8cc4bf64653153d58d05d064c
moran-x <moran.x@gmail.com>
2025-02-06 15:21:08 +0700

    Update ledwindow.py
    add new function

--- pyscript/ledwindow.py
+++ pyscript/ledwindow.py
@@ -1,5 +1,5 @@
-led_lamps_ip = ["10.1.30.44", "10.1.30.40"]
-led_lamps_name = ["LedWiFiTree", "LedWiFiWindow"]
+led_lamps_ip = ["10.1.30.40"]
+led_lamps_name = ["LedWiFiWindow"]
 
 import json
 import subprocess
@@ -34,13 +34,62 @@ def get_params():
 def LedWindow_on_off(led_ip=None):
     for idx, led_lamp_ip in enumerate(led_lamps_ip):
         if led_ip==led_lamp_ip:
-            cmd = "binary_sensor."+led_lamps_name[idx].lower()+"_ps"
-            sensor_state = state.get(cmd)
+            cmd_ps = "switch."+led_lamps_name[idx].lower()+"_ps"
+            sensor_state = state.get(cmd_ps)
             if sensor_state == "on":
                 command = json.dumps({'e': 'cmd', 'd': '$1 0'})
             else:
                 command = json.dumps({'e': 'cmd', 'd': '$1 1'})
             command_ws = 'echo \''+command+'\' | /config/websocat -n1 ws://'+led_lamp_ip+'/ws'
+            #log.info(command_ws)
             subprocess.Popen(command_ws, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, text=True)
             get_params()
 
+@service
+def LedWindow_brightness(led_ip=None, brightness=None):
+    for idx, led_lamp_ip in enumerate(led_lamps_ip):
+        if led_ip==led_lamp_ip:
+            cmd_br = "light."+led_lamps_name[idx].lower()+"_br"
+            cmd_ps = "switch."+led_lamps_name[idx].lower()+"_ps"
+            sensor_state = state.get(cmd_ps)
+            if sensor_state == "on":
+                command = json.dumps({'e': 'cmd', 'd': '$4 0 '+str(brightness)})
+                command_ws = 'echo \''+command+'\' | /config/websocat -n1 ws://'+led_lamp_ip+'/ws'
+                subprocess.Popen(command_ws, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, text=True)
+                get_params()
+
+
+@service
+def LedWindow_random_effect(led_ip=None):
+    for idx, led_lamp_ip in enumerate(led_lamps_ip):
+        if led_ip==led_lamp_ip:
+            cmd_rm = "switch."+led_lamps_name[idx].lower()+"_rm"
+            cmd_ps = "switch."+led_lamps_name[idx].lower()+"_ps"
+            sensor_state_ps = state.get(cmd_ps)
+            sensor_state_rm = state.get(cmd_rm)
+            if sensor_state_ps == "on":
+                if sensor_state_rm == "on":
+                    command = json.dumps({'e': 'cmd', 'd': '$16 5 0'})
+                else:
+                    command = json.dumps({'e': 'cmd', 'd': '$16 5 1'})
+                command_ws = 'echo \''+command+'\' | /config/websocat -n1 ws://'+led_lamp_ip+'/ws'
+                subprocess.Popen(command_ws, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, text=True)
+                get_params()
+
+
+@service
+def LedWindow_auto_mode(led_ip=None):
+    for idx, led_lamp_ip in enumerate(led_lamps_ip):
+        if led_ip==led_lamp_ip:
+            cmd_dm = "switch."+led_lamps_name[idx].lower()+"_dm"
+            cmd_ps = "switch."+led_lamps_name[idx].lower()+"_ps"
+            sensor_state_ps = state.get(cmd_ps)
+            sensor_state_dm = state.get(cmd_dm)
+            if sensor_state_ps == "on":
+                if sensor_state_dm == "on":
+                    command = json.dumps({'e': 'cmd', 'd': '$16 0'})
+                else:
+                    command = json.dumps({'e': 'cmd', 'd': '$16 1'})
+                command_ws = 'echo \''+command+'\' | /config/websocat -n1 ws://'+led_lamp_ip+'/ws'
+                subprocess.Popen(command_ws, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, text=True)
+                get_params()
