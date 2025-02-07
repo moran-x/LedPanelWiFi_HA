d923a19a80321868f4c05f5a7fe69e1ecddc4360
moran-x <moran.x@gmail.com>
2025-02-07 19:05:33 +0700

    Update ledwindow.py

--- pyscript/ledwindow.py
+++ pyscript/ledwindow.py
@@ -1,12 +1,16 @@
+import json
+import paho.mqtt.client as mqtt
+from websocket import create_connection
+
+# IP адрес ленты
 led_lamps_ip = ["10.1.30.40"]
+# Название ленты
 led_lamps_name = ["LedWiFiWindow"]
 
-import json
-import subprocess
-import paho.mqtt.client as mqtt
+mqtt_username = "mqtt_user" # Имя пользователя MQTT
+mqtt_password =  "pass" # Пароль пользователя MQTT
 
-mqtt_username = "mqtt_user"
-mqtt_password =  "pass"
+# Дальше лучше ничего не менять
 mqtt_host = "127.0.0.1"
 mqtt_port = 1883
 
@@ -17,33 +21,33 @@ client.connect(mqtt_host, mqtt_port)
 @service
 def get_params():
     for idx, led_lamp_ip in enumerate(led_lamps_ip):
-        
+        ws = create_connection("ws://"+led_lamp_ip+"/ws")
         params = ["VR", "HN", "BR", "PS", "DM", "RM", "EF", "SM", "EN", "UP", "FM", "IP"]
 
         for param in params:
             command = json.dumps({'e': 'cmd', 'd': '$6 7|'+param})
-            command_ws = 'echo \''+command+'\' | /config/websocat -n1 ws://'+led_lamp_ip+'/ws'
-            result = subprocess.Popen(command_ws, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, text=True)
-            line = result.stdout.readline()
-            x = json.loads(line)
+            ws.send(command)
+            result =  ws.recv()
+            x = json.loads(result)
             y = json.loads(x["d"])
             client.publish(led_lamps_name[idx]+'/'+param, payload=str(y[param]), qos=0, retain=False)
+    ws.close()
 
 
 @service
 def LedWindow_on_off(led_ip=None):
     for idx, led_lamp_ip in enumerate(led_lamps_ip):
         if led_ip==led_lamp_ip:
+            ws = create_connection("ws://"+led_lamp_ip+"/ws")
             cmd_ps = "switch."+led_lamps_name[idx].lower()+"_ps"
             sensor_state = state.get(cmd_ps)
             if sensor_state == "on":
                 command = json.dumps({'e': 'cmd', 'd': '$1 0'})
             else:
                 command = json.dumps({'e': 'cmd', 'd': '$1 1'})
-            command_ws = 'echo \''+command+'\' | /config/websocat -n1 ws://'+led_lamp_ip+'/ws'
-            #log.info(command_ws)
-            subprocess.Popen(command_ws, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, text=True)
+            ws.send(command)
             get_params()
+            ws.close()        
 
 @service
 def LedWindow_brightness(led_ip=None, brightness=None):
@@ -53,10 +57,11 @@ def LedWindow_brightness(led_ip=None, brightness=None):
             cmd_ps = "switch."+led_lamps_name[idx].lower()+"_ps"
             sensor_state = state.get(cmd_ps)
             if sensor_state == "on":
+                ws = create_connection("ws://"+led_lamp_ip+"/ws")
                 command = json.dumps({'e': 'cmd', 'd': '$4 0 '+str(brightness)})
-                command_ws = 'echo \''+command+'\' | /config/websocat -n1 ws://'+led_lamp_ip+'/ws'
-                subprocess.Popen(command_ws, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, text=True)
+                ws.send(command)
                 get_params()
+                ws.close()
 
 
 @service
@@ -68,13 +73,14 @@ def LedWindow_random_effect(led_ip=None):
             sensor_state_ps = state.get(cmd_ps)
             sensor_state_rm = state.get(cmd_rm)
             if sensor_state_ps == "on":
+                ws = create_connection("ws://"+led_lamp_ip+"/ws")
                 if sensor_state_rm == "on":
                     command = json.dumps({'e': 'cmd', 'd': '$16 5 0'})
                 else:
                     command = json.dumps({'e': 'cmd', 'd': '$16 5 1'})
-                command_ws = 'echo \''+command+'\' | /config/websocat -n1 ws://'+led_lamp_ip+'/ws'
-                subprocess.Popen(command_ws, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, text=True)
+                ws.send(command)
                 get_params()
+                ws.close()
 
 
 @service
@@ -86,10 +92,11 @@ def LedWindow_auto_mode(led_ip=None):
             sensor_state_ps = state.get(cmd_ps)
             sensor_state_dm = state.get(cmd_dm)
             if sensor_state_ps == "on":
+                ws = create_connection("ws://"+led_lamp_ip+"/ws")
                 if sensor_state_dm == "on":
                     command = json.dumps({'e': 'cmd', 'd': '$16 0'})
                 else:
                     command = json.dumps({'e': 'cmd', 'd': '$16 1'})
-                command_ws = 'echo \''+command+'\' | /config/websocat -n1 ws://'+led_lamp_ip+'/ws'
-                subprocess.Popen(command_ws, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, text=True)
+                ws.send(command)
                 get_params()
+                ws.close()
